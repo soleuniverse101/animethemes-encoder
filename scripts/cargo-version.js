ec69b71854a993ff2277c0aa59ed328d8e78a1a4
soleuniverse <dev@soleuniverse.me>
2026-06-02 02:20:38 +0200

    chore: fix Cargo.lock version not bumped in release action

--- scripts/cargo-version.js
+++ scripts/cargo-version.js
@@ -1,6 +1,8 @@
 import { execSync } from "node:child_process";
 
 const version = execSync("pnpm pkg get version").toString();
+const packageName = execSync("pnpm pkg get name").toString();
 execSync("cargo bump --manifest-path src-tauri/Cargo.toml " + version);
+execSync("cargo update --manifest-path src-tauri/Cargo.toml " + packageName);
 
 console.log("Cargo.toml version bumped to " + version);
