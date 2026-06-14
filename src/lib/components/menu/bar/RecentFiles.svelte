5bde0936579e332195ce98a3fd92f9561b76f63c
soleuniverse <dev@soleuniverse.me>
2026-06-14 14:55:30 +0200

    feat: button to remove recent files

--- src/lib/components/menu/bar/RecentFiles.svelte
+++ src/lib/components/menu/bar/RecentFiles.svelte
@@ -1,23 +1,35 @@
 <script lang="ts">
   import { commands } from "$lib/app/commands";
   import { getApp } from "$lib/app/index.svelte";
+  import Icon from "@iconify/svelte";
   import { Menubar } from "bits-ui";
   import MenuItem from "./MenuItem.svelte";
 
   const app = getApp();
+  const { recent } = $derived(app.config);
 </script>
 
 {#if app.config.recent.length > 0}
   <Menubar.Separator />
   <Menubar.Group>
     <Menubar.GroupHeading>Recent files</Menubar.GroupHeading>
-    {#each app.config.recent as path}
-      <MenuItem
-        // TODO rearrange based on window size
-        class="truncate max-w-lg"
-        title={path}
-        action={{ type: "button", callback: () => commands("mpvView").importFile(path) }}
-      />
+    {#each recent as path, index}
+      <!-- TODO set variable for hover color -->
+      <div class="flex justify-between hover:bg-primary-400">
+        <MenuItem
+          // TODO rearrange based on window size
+          class="truncate max-w-lg"
+          title={path}
+          action={{ type: "button", callback: () => commands("mpvView").importFile(path) }}
+        />
+        <!-- TODO use variables to set height & width (probably same as line height) -->
+        <button
+          onclick={() => recent.splice(index, 1)}
+          class="h-6 w-6 flex items-center justify-center bg-none cursor-auto"
+        >
+          <Icon icon="mdi:remove" />
+        </button>
+      </div>
     {/each}
   </Menubar.Group>
 {/if}
