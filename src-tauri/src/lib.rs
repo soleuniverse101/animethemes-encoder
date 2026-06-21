3414285d215d1d4da5a345661f9d145ad9ec5172
soleuniverse <dev@soleuniverse.me>
2026-06-21 06:01:08 +0200

    feat: single instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,6 +10,15 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let main = app.get_webview_window("main").expect("no main window");
+            if !main.is_visible().unwrap() {
+                for webview in app.webview_windows().values() {
+                    webview.show().unwrap();
+                }
+            }
+            main.set_focus().unwrap();
+        }))
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_os::init())
