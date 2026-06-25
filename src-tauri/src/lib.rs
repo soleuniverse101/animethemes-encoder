bd3be55c7a822927b85b8eff8a97a032680b6dac
soleuniverse <dev@soleuniverse.me>
2026-06-25 20:58:19 +0200

    refactor: move unused imports & variables in conditional block

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,4 @@
-use serde::Deserialize;
-use tauri::{Listener, Manager};
+use tauri::Manager;
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -9,7 +8,7 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    let builder = tauri::Builder::default()
         .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
             let main = app.get_webview_window("main").expect("no main window");
             if !main.is_visible().unwrap() {
@@ -26,29 +25,31 @@ pub fn run() {
         .plugin(tauri_plugin_libmpv::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .setup(|app| {
-            // Open DevTools console for every webview in development
-            #[cfg(debug_assertions)]
-            {
-                let app_handle = app.handle().clone();
-                app.listen("tauri://webview-created", move |event| {
-                    #[derive(Clone, Deserialize)]
-                    struct WebviewCreatedPayload {
-                        label: String,
-                    }
-                    if let Ok(WebviewCreatedPayload { label }) =
-                        serde_json::from_str(event.payload())
-                    {
-                        app_handle
-                            .get_webview_window(&label)
-                            .unwrap()
-                            .open_devtools();
-                    }
-                });
+        .invoke_handler(tauri::generate_handler![greet]);
+
+    #[cfg(debug_assertions)]
+    let builder = builder.setup(|app| {
+        // Open DevTools console for every webview in development
+        use serde::Deserialize;
+        use tauri::Listener;
+
+        let app_handle = app.handle().clone();
+        app.listen("tauri://webview-created", move |event| {
+            #[derive(Clone, Deserialize)]
+            struct WebviewCreatedPayload {
+                label: String,
             }
-            Ok(())
-        })
+            if let Ok(WebviewCreatedPayload { label }) = serde_json::from_str(event.payload()) {
+                app_handle
+                    .get_webview_window(&label)
+                    .unwrap()
+                    .open_devtools();
+            }
+        });
+        Ok(())
+    });
+
+    builder
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
